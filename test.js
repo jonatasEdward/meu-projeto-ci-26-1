b67fed5abc5f0c9a505fd834643d77c508f69eb4
Jonatas Edward <jonatas.edo@gmail.com>
2026-04-28 21:24:43 -0300

    Create test.js
    Add test

---
+++ test.js
@@ -0,0 +1,11 @@
+const saudacao = require('./hello');
+// Teste com erro intencional
+const resultado = saudacao("Mundo");
+const esperado = "Hello, Mundo!"; 
+️
+if (resultado !== esperado) {
+  console.error(` FALHA: esperado "${esperado}", mas 
+recebeu "${resultado}"`);
+  process.exit(1); // Faz o CI falhar
+}
+console.log(" Todos os testes passaram!");
